a400d61b2085a41be82d9bbe61b827944a9b5925
han45678 <han0910812144@gmail.com>
2021-08-31 00:02:15 +0800

    行事曆增加

--- src/views/records.vue
+++ src/views/records.vue
@@ -592,42 +592,7 @@
                   </div>
                 </div>
                 <div v-show="options === 3">
-                  <div id="calendar">
-                    <!-- 行事曆 <v-calendar
-                      class="custom-calendar max-w-full"
-                      :masks="masks"
-                      :attributes="attributes"
-                      disable-page-swipe
-                      is-expanded
-                    >
-                      <template v-slot:day-content="{ day, attributes }">
-                        <div class="flex flex-col h-full z-10 overflow-hidden">
-                          <span class="day-label text-sm text-gray-900">{{
-                            day.day
-                          }}</span>
-                          <div
-                            class="flex-grow overflow-y-auto overflow-x-auto"
-                          >
-                            <p
-                              v-for="attr in attributes"
-                              :key="attr.key"
-                              class="
-                                text-xs
-                                leading-tight
-                                rounded-sm
-                                p-1
-                                mt-0
-                                mb-1
-                              "
-                              :class="attr.customData.class"
-                            >
-                              {{ attr.customData.title }}
-                            </p>
-                          </div>
-                        </div>
-                      </template>
-                    </v-calendar> -->
-                  </div>
+                  <vc-calendar is-expanded />
                 </div>
                 <div v-show="options === 4">
                   <div id="task">
